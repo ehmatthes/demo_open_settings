6668a7b5fd121ed29d9bb22018fb14c776127be0
eric <eric@example.com>
2023-11-04 20:57:37 -0800

    Works on Windows.

--- open_preferences.py
+++ open_preferences.py
@@ -1,8 +1,11 @@
 """Open the operating system's System Preferences,
 or equivalent."""
 
-import subprocess, shlex
+import os, platform, subprocess, shlex
 
-cmd = "open '/System/Applications/System Settings.app'"
-cmd_parts = shlex.split(cmd)
-subprocess.run(cmd_parts)
+if platform.system() == "Darwin":
+    cmd = "open '/System/Applications/System Settings.app'"
+    cmd_parts = shlex.split(cmd)
+    subprocess.run(cmd_parts)
+elif platform.system() == 'Windows':
+    cmd = os.system("control")
